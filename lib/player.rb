b2361f55f05031ceb92b5ea1c404d5c1fcd074a2
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-03-31 16:27:22 -0500

    Add player.rb methods

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,26 @@
 class Player
   attr_reader :name
   
-  def initialize(name)
+  def initialize(name, id)
     @name = name
+    @id = id
+  end
+
+  def symb
+    if @id == 1
+      "X"
+    elsif @id == 2
+      "O"
+    end
+  end
+
+  @played_cells = []
+
+  def save_play(cell)
+    @played_cells << cell
+  end
+
+  def played_cells
+    @played_cells
   end
 end
