0a7ec5c0a5d6702e300e923db9f143ac632be2a8
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-04-09 12:01:00 -0500

    add specs for board creation and board structure

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,22 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
+  describe '.create_board' do
+    it 'creates a board hash with keys and values' do
+      Board.create_board
+      expect(Board.board).to be_a(Hash)
+    end
+  end
 
+  describe '.board' do
+    it 'the board contains keys and values from 1 to 9' do
+      board = Board.board
+      expect(board).to include((1..9))
+    end
+
+    it 'the board length is 9' do
+      board = Board.board
+      expect(board.length).to eql(9)
+    end
+  end
 end
